1180e593b0d7a31be7c0b5d82f58263e1cf3cdce
Tridibesh Nag <tnag97@gmail.com>
2023-01-12 13:27:59 +0530

    knob animation text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { KnobModule } from "primeng/knob";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, 
+    KnobModule,
     FormsModule
   ],
   providers: [],
